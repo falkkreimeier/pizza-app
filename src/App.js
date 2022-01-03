5162ea000aaf860c12f40b5fb5a61173890b7d92
Falk Kreimeier <f.kreimeier@gmx.net>
2022-01-03 23:02:44 +0100

    build up Layout component and delete unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import styled from "styled-components";
-import Footer from "./components/Footer";
 import LandingPage from "./components/Landingpage/LandingPage";
 import OrderingOverview from "./components/Ordering/OrderingOverview";
 import { Routes, Route } from "react-router-dom";
@@ -9,37 +7,23 @@ import Pizza from "./components/Ordering/Pizza";
 import Pasta from "./components/Ordering/Pasta";
 import Dessert from "./components/Ordering/Dessert";
 import Drinks from "./components/Ordering/Drinks";
-import HeaderRoutes from "./components/HeaderRoutes";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <Wrapper>
-      <HeaderRoutes />
-      <Main>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/karte" element={<OrderingOverview />} />
-          <Route path="/vorspeisen" element={<Appetizers />} />
-          <Route path="/salate" element={<Salads />} />
-          <Route path="/pizza" element={<Pizza />} />
-          <Route path="/pasta" element={<Pasta />} />
-          <Route path="/nachtisch" element={<Dessert />} />
-          <Route path="/getraenke" element={<Drinks />} />
-        </Routes>
-      </Main>
-
-      <Footer />
-    </Wrapper>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/karte" element={<OrderingOverview />} />
+        <Route path="/vorspeisen" element={<Appetizers />} />
+        <Route path="/salate" element={<Salads />} />
+        <Route path="/pizza" element={<Pizza />} />
+        <Route path="/pasta" element={<Pasta />} />
+        <Route path="/nachtisch" element={<Dessert />} />
+        <Route path="/getraenke" element={<Drinks />} />
+      </Route>
+    </Routes>
   );
 }
-
-const Wrapper = styled.section`
-  margin: 0 auto;
-  height: var(--app-height);
-  width: var(--app-width);
-  background-color: yellow;
-`;
-
-const Main = styled.main``;
 
 export default App;
